52d5f0a7798e4a6b3de16fcd2ce3507bbcf9b80c
Matheus Correa <mvcmatheus12@gmail.com>
2018-09-09 20:29:24 -0300

    Carrinho de Compras

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -9,6 +9,46 @@
 		const SESSION = "User";
 		const SECRET = "Mvcsoftwaresphp";
 
+		public static function getFromSession()
+		{
+			$user = new User();
+
+			if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0){
+
+				$user->setData($_SESSION[User::SESSION]);
+
+			}
+
+			return $user;
+		}
+
+		public static function checkLogin($inadmin = true)
+		{
+			if(
+				!isset($_SESSION[User::SESSION]) 
+				|| 
+				!$_SESSION[User::SESSION] 
+				|| 
+				!(int)$_SESSION[User::SESSION]["iduser"] > 0 
+			  ){
+				//Não esta logado
+					return false;
+			   }else{
+
+			   			if($inadmin === true && (bool) $_SESSION[User::SESSION]['inadmin'] === true)
+			   			{
+			   				return true;
+			   			}else if($inadmin === false){
+
+			   				return true;
+			   			}else{
+
+			   				return false;
+			   			}
+
+					}
+		}
+
 		public static function login($login, $password)
 		{
 
@@ -47,16 +87,7 @@
 
 		public static function verifyLogin($inadmin = true){
 
-			if(
-
-				!isset($_SESSION[User::SESSION]) 
-				|| 
-				!$_SESSION[User::SESSION] 
-				|| 
-				!(int)$_SESSION[User::SESSION]["iduser"] > 0 
-				|| (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-
-			){
+			if(User::checkLogin($inadmin)){
 				Header("Location: /admin/login");
 				exit;
 			}
